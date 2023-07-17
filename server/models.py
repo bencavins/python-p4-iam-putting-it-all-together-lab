8fb8306619a92427b1f870b492a676a06d6d8376
Ben Cavins <ben.cavins@flatironschool.com>
2023-07-17 11:31:50 -0400

    add db models

--- server/models.py
+++ server/models.py
@@ -6,9 +6,41 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    # one to many with Recipe
+    recipies = db.relationship('Recipe', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Cannot access password hash!')
+    
+    @password_hash.setter
+    def password_hash(self, new_pass):
+        p_hash = bcrypt.generate_password_hash(new_pass.encode('utf-8'))
+        self._password_hash = p_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # TODO validate instructions (>= 50 chars)
+
+    def __repr__(self):
+        return f"<Recipe {self.title}>"
