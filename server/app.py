b328f5501a1d4c429dacc46864341f8fcf8a1d29
Ben Cavins <ben.cavins@flatironschool.com>
2023-07-17 13:33:57 -0400

    add auth and recipe routes

--- server/app.py
+++ server/app.py
@@ -54,6 +54,57 @@ def signup():
     # return user as JSON, status code 201
     return new_user.to_dict(), 201
 
+@app.get('/check_session')
+def check_session():
+    # get user_id from browser cookies
+    user_id = session.get('user_id')
+    # check for user in db
+    user = User.query.filter(User.id == user_id).first()
+
+    if not user:
+        # user doesn't exist, return 401 (unauthorized)
+        return {'error': 'Unauthorized'}, 401
+    
+    # user exists, return user as JSON, status code 200
+    return user.to_dict(), 200
+
+@app.post('/login')
+def login():
+    # get JSON from request
+    data = request.get_json()
+    # query db by username
+    user = User.query.filter(
+        User.username == data['username']
+    ).first()
+    
+    if not user or not user.authenticate(data['password']):
+        # user doesn't exist or password doesn't match, return 401
+        return {'error': 'Login failed'}, 401
+    
+    # login success, add cookie to broswer
+    session['user_id'] = user.id
+    return user.to_dict(), 201
+
+@app.delete('/logout')
+def logout():
+    # check if user_id cookie is set
+    user_id = session.get('user_id')
+
+    if not user_id:
+        # no cookie is set, return 401
+        return {'error': 'User is not logged in'}, 401
+    
+    # delete the cookie
+    session.pop('user_id')
+    # return 204 (no content)
+    return {}, 204
+
+@app.get('/recipes')
+def get_all_recipes():
+    recipes = Recipe.query.all()
+    data = [r.to_dict() for r in recipes]
+    return data, 200
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
